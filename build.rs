e2a7fb65c199e1024599014a8500375d0dd60a51
Oleg Khryptul <okreptul@yahoo.com>
2018-05-11 14:00:21 +0200

    Initial implementation for windows

--- build.rs
+++ build.rs
@@ -3,34 +3,38 @@ extern crate cc;
 extern crate walkdir;
 
 mod build_linux;
+pub mod build_utils;
+mod build_windows;
 
 use std::env;
 use std::path::PathBuf;
 
 fn main() {
-    //if cfg!(feature = "build") {
-    if env::var("TARGET").unwrap().contains("linux") {
-        build_linux::build(false);
-    } else {
-        panic!("Unsupported platform!");
+    if cfg!(feature = "build") {
+        if env::var("TARGET").unwrap().contains("linux") {
+            build_linux::build();
+        } else if env::var("TARGET").unwrap().contains("windows") {
+            build_windows::build();
+        } else {
+            panic!("Unsupported platform!");
+        }
     }
-    //}
 
-    //if cfg!(feature = "bind") {
-    println!("cargo:rustc-link-lib=bullet3");
+    if cfg!(feature = "bind") {
+        println!("cargo:rustc-link-lib=bullet3");
 
-    let bindings = bindgen::Builder::default()
-        .clang_arg("-x")
-        .clang_arg("c++")
-        .clang_arg("-Ibullet3/src")
-        .header("bullet3.h")
-        .generate()
-        .expect("Unable to generate bindings");
+        let bindings = bindgen::Builder::default()
+            .clang_arg("-x")
+            .clang_arg("c++")
+            .clang_arg("-Ibullet3/src")
+            .header("bullet3.h")
+            .generate()
+            .expect("Unable to generate bindings");
 
-    //let out_path = PathBuf::from(env::var("OUT_DIR").unwrap());
-    let out_path = PathBuf::from("src/");
-    bindings
-        .write_to_file(out_path.join("bullet3.rs"))
-        .expect("Couldn't write bindings!");
-    //}
+        //let out_path = PathBuf::from(env::var("OUT_DIR").unwrap());
+        let out_path = PathBuf::from("src/");
+        bindings
+            .write_to_file(out_path.join("bullet3.rs"))
+            .expect("Couldn't write bindings!");
+    }
 }
