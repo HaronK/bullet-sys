89406a8c8b04bef596f9e92092978fa7e5054ab5
Oleg Khryptul <okreptul@yahoo.com>
2018-05-16 14:50:14 +0200

    Using cmake crate instead of cc

--- build.rs
+++ build.rs
@@ -1,23 +1,25 @@
 extern crate bindgen;
 extern crate cc;
+extern crate cmake;
 extern crate walkdir;
 
-mod build_linux;
-pub mod build_utils;
-mod build_windows;
-
+use cmake::Config;
 use std::env;
 use std::path::PathBuf;
 
 fn main() {
     if cfg!(feature = "build") {
-        if env::var("TARGET").unwrap().contains("linux") {
-            build_linux::build();
-        } else if env::var("TARGET").unwrap().contains("windows") {
-            build_windows::build();
-        } else {
-            panic!("Unsupported platform!");
-        }
+        let dst = Config::new("bullet3")
+            .define("BUILD_PYBULLET", "OFF")
+            .define("BUILD_UNIT_TESTS", "OFF")
+            .define("BUILD_CPU_DEMOS", "OFF")
+            .define("BUILD_BULLET2_DEMOS", "OFF")
+            .define("USE_DOUBLE_PRECISION", "ON")
+            .generator("Visual Studio 14 2015 Win64")
+            .build();
+
+        // println!("cargo:rustc-link-search=native={}", dst.display());
+        // println!("cargo:rustc-link-lib=static=bullet3");
     }
 
     if cfg!(feature = "bind") {
