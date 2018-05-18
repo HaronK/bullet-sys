10a7de4fb66c6f35f9d629e983790baab2ba204a
Oleg Khryptul <okreptul@yahoo.com>
2018-05-18 23:50:34 +0200

    Update lib.rs

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,14 @@
-#[cfg(test)]
-mod tests {
-    #[test]
-    fn it_works() {
-        assert_eq!(2 + 2, 4);
-    }
-}
+#![warn(unused_extern_crates)]
+
+#[cfg(target_os = "linux")]
+mod bullet_linux;
+
+#[cfg(target_os = "linux")]
+pub use bullet_linux::*;
+
+#[cfg(target_os = "windows")]
+mod bullet_windows;
+
+#[cfg(target_os = "windows")]
+pub use bullet_windows::*;
+
